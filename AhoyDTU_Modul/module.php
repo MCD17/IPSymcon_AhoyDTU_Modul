32bbfbccc56ce039639f2bbb5eace3d2375fd228
MCD17 <topsecret17@gmx.de>
2024-02-04 22:31:58 +0100

    Squashed commit of the following:
    commit e164966de834d2607f2e3d0dbea75a09d178f79e
    Author: MCD17 <topsecret17@gmx.de>
    Date:   Sun Feb 4 22:31:48 2024 +0100
    
        first variable
    
    commit d2f4ae3893b280ae8c5808a2428a10ecc0af2a26
    Author: MCD17 <topsecret17@gmx.de>
    Date:   Sun Feb 4 22:25:41 2024 +0100
    
        First module
    
    commit f3ef70fbb9e0d83c8c9f4d15bc3132e2449fe057
    Author: MCD17 <topsecret17@gmx.de>
    Date:   Sun Feb 4 22:08:44 2024 +0100
    
        Update library.json
    
    commit 989adfefa2d55ee86235362c057083c121b3aaec
    Author: MCD17 <topsecret17@gmx.de>
    Date:   Sun Feb 4 22:06:43 2024 +0100
    
        first commit

---
+++ AhoyDTU_Modul/module.php
@@ -0,0 +1,28 @@
+<?
+    // Klassendefinition
+    class AhoyDTU_Instance extends IPSModule {
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+            
+            $this->RegisterVariableString("mqtt_main_topic", "Main MQTT Topic für die DTU Instanz");
+        }
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+            $this->SetValueString("mqtt_main_topic", "Hallo Welt!");
+        }
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            // Selbsterstellter Code
+        }
+    }
+?>
